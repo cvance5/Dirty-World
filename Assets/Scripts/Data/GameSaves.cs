630bd935fd43d5ba84919354efdca1d4f10cf2b2
cvance5 <32501792+cvance5@users.noreply.github.com>
2018-07-28 21:31:06 -0400

    [Game Systems : Data Saving] Recreate the Top Level Systems

---
+++ Assets/Scripts/Data/GameSaves.cs
@@ -0,0 +1,66 @@
+﻿using Data.IO;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Data
+{
+    public static class GameSaves
+    {
+        private static List<string> _savedGames;
+        public static List<string> SavedGames => new List<string>(_savedGames);
+        public static bool HasSavedData => _savedGames.Count > 0;
+
+        public static string CurrentGame { get; private set; }
+
+        private static List<string> _currentGameData = new List<string>();
+        public static bool HasGameData(string dataName) => _currentGameData.Contains(dataName);
+
+        public static void Refresh()
+        {
+            _savedGames = DataReader.FindAllDirectories(DataTypes.SaveGames);
+        }
+
+        public static void SetCurrent(string currentGame)
+        {
+            CurrentGame = currentGame;
+
+            if (_savedGames.Contains(CurrentGame))
+            {
+                _currentGameData.Clear();
+
+                _currentGameData = DataReader.FindAllFiles(DataTypes.CurrentGame);
+            }
+        }
+
+        public static void SaveDirty()
+        {
+            if (!_savedGames.Contains(CurrentGame))
+            {
+                Directory.CreateDirectory(Paths.ToPath(DataTypes.CurrentGame));
+            }
+
+            var filesToWrite = GameState.SerializeAll();
+
+            if (filesToWrite.Count == 0)
+            {
+                _log.Info("No dirty data to save.");
+                return;
+            }
+
+            foreach (var kvp in filesToWrite)
+            {
+                _log.Info($"Updating {kvp.Key}...");
+                var writeLocation = Paths.ToPath(kvp.Key, DataTypes.CurrentGame);
+                DataWriter.Write(writeLocation, kvp.Value);
+            }
+
+            _log.Info($"Save complete, updated {filesToWrite.Count} files.");
+
+            GameState.Clear();
+
+            Refresh();
+        }
+
+        private static readonly Log _log = new Log("GameSaves");
+    }
+}
---
+++ Assets/Scripts/Data/GameState.cs
@@ -0,0 +1,55 @@
+﻿using Data.Serialization;
+using System;
+using System.Collections.Generic;
+using WorldObjects;
+
+namespace Data
+{
+    public static class GameState
+    {
+        private static readonly Dictionary<IntVector2, Chunk>
+             _dirtyChunks = new Dictionary<IntVector2, Chunk>();
+
+        public static void Initialize()
+        {
+            Chunk.OnChunkChanged += LogDirty;
+        }
+
+        public static void LogDirty(Chunk dirtyChunk)
+        {
+            if (!_dirtyChunks.ContainsKey(dirtyChunk.Position))
+            {
+                _dirtyChunks.Add(dirtyChunk.Position, dirtyChunk);
+            }
+        }
+
+        public static Dictionary<string, string> SerializeAll()
+        {
+            var fileToWrite = new Dictionary<string, string>();
+
+            try
+            {
+                foreach (var kvp in _dirtyChunks)
+                {
+                    var fileName = kvp.Key.ToString();
+                    var data = new SerializableChunk(kvp.Value).Serialize();
+                    fileToWrite.Add(fileName, data);
+                }
+            }
+            catch (Exception e)
+            {
+                _log.Error($"Failed to serialize data during a save with exception {e.Message}.  Aborting.");
+                return new Dictionary<string, string>();
+            }
+
+            return fileToWrite;
+        }
+
+        public static void Clear()
+        {
+            _dirtyChunks.Clear();
+        }
+
+        private static readonly Log _log = new Log("GameState");
+    }
+}
