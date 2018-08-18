c0934800a2d891fbd54cb8e73a72058544307ba0
cvance5 <32501792+cvance5@users.noreply.github.com>
2018-08-18 16:06:05 -0400

    [UI] Make UIObjects Aware of ApplicationQuit
    Some will have destruction effects, and, as usual, destruction effects often should not occur when the application is quitting.

--- Assets/Scripts/UI/UIObject.cs
+++ Assets/Scripts/UI/UIObject.cs
@@ -4,6 +4,13 @@ namespace UI
 {
     public abstract class UIObject : MonoBehaviour
     {
+        protected bool _isApplicationQuitting;
+
         public abstract void SetVisible(bool isVisible);
+
+        protected void OnApplicationQuit()
+        {
+            _isApplicationQuitting = true;
+        }
     }
 }
