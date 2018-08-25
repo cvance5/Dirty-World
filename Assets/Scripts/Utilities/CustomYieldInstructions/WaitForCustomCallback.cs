20c41502b7b24b05ac612615963a9324bfa04f30
cvance5 <32501792+cvance5@users.noreply.github.com>
2018-08-25 18:43:12 -0400

    [Utilities] Add CustomYieldInstructions Namespace
    Add WaitForCustomCallback
    Add WaitForObjectDestroyed

---
+++ Assets/Scripts/Utilities/CustomYieldInstructions/WaitForCustomCallback.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class WaitForCustomCallback : CustomYieldInstruction
+{
+    public override bool keepWaiting => _keepWaiting;
+    private bool _keepWaiting = true;
+
+    public WaitForCustomCallback() { }
+
+    public void Callback()
+    {
+        _keepWaiting = false;
+    }
+}
---
+++ Assets/Scripts/Utilities/CustomYieldInstructions/WaitForObjectDestroyed.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Utilities.CustomYieldInstructions
+{
+    public class WaitForObjectDestroyed : CustomYieldInstruction
+    {
+        public override bool keepWaiting => _target != null;
+
+        private readonly Object _target;
+
+        public WaitForObjectDestroyed(Object target)
+        {
+            _target = target;
+        }
+    }
+}
