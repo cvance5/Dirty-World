3b911a9605177101bcdebc2a8f2639469a8befa3
cvance5 <32501792+cvance5@users.noreply.github.com>
2018-08-25 18:54:25 -0400

    [Utilities] Add FrameTimer to Timekeeper
    For when we need to wait X frames, regardless of real time. Useful for anything Unity guarantees to happen by next frame, like scene loading or animation effects.

--- Assets/Scripts/Timekeeper.cs
+++ Assets/Scripts/Timekeeper.cs
@@ -9,9 +9,25 @@ public class Timekeeper : Singleton<Timekeeper>
         return Instance.StartCoroutine(WaitForTimer(delay, callback));
     }
 
+    public static Coroutine SetTimer(int frames, Action callback)
+    {
+        return Instance.StartCoroutine(WaitForFrames(frames, callback));
+    }
+
     private static IEnumerator WaitForTimer(float delay, Action callback)
     {
         yield return new WaitForSeconds(delay);
+        callback?.Invoke();
+    }
+
+    private static IEnumerator WaitForFrames(int frames, Action callback)
+    {
+        while (frames > 0)
+        {
+            yield return new WaitForEndOfFrame();
+            frames--;
+        }
+
         callback?.Invoke();
     }
 }
