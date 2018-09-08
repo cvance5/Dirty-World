8d9615fb585bbf2498e66b946a4a620c27c29302
cvance5 <32501792+cvance5@users.noreply.github.com>
2018-09-08 16:42:58 -0400

    [Debug] Add Debug Controller for Debug-Only Inputs

---
+++ Assets/Scripts/DebugController.cs
@@ -0,0 +1,16 @@
+﻿using Actors.Player;
+using UnityEngine;
+
+public class DebugController : Singleton<DebugController>
+{
+#if UNITY_EDITOR
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftBracket))
+        {
+            FindObjectOfType<PlayerData>().ApplyDamage(10000);
+        }
+    }
+#endif
+}
