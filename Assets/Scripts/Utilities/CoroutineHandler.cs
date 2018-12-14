6f654c85bdd247fcbe5682d093761eb8d5ff4580
cvance5 <32501792+cvance5@users.noreply.github.com>
2018-12-14 17:33:26 -0500

    [Utilities] Add Singleton to Initialize Coroutines For Static Classes
    Is this bad practice?  Should static classes just not have coroutines?  But they are so useful...

---
+++ Assets/Scripts/Utilities/CoroutineHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections;
+
+namespace Utilities
+{
+    public class CoroutineHandler : Singleton<CoroutineHandler>
+    {
+        public static void RegisterCoroutine(Func<IEnumerator> coroutine) => Instance.StartCoroutine(coroutine());
+    }
+}
