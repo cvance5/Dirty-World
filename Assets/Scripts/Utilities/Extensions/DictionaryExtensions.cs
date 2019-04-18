55a9a539bd3508209b042850f21372125ef0c24d
cvance5 <32501792+cvance5@users.noreply.github.com>
2019-04-18 19:34:51 -0400

    [Utility] Add RemoveAll Via Predicate Dictionary Function

---
+++ Assets/Scripts/Utilities/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class DictionaryExtensions
+{
+    public static void RemoveAll<T, U>(this Dictionary<T, U> dict, Func<KeyValuePair<T, U>, bool> predicate)
+    {
+        var newDict = new Dictionary<T, U>(dict);
+        foreach (var kvp in newDict)
+        {
+            if (predicate(kvp))
+            {
+                dict.Remove(kvp.Key);
+            }
+        }
+    }
+}
