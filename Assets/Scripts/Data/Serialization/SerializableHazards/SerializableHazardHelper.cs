0b98549d0b4b5fa5a4dd64b2eb12620841a35cb8
cvance5 <32501792+cvance5@users.noreply.github.com>
2019-04-20 18:54:21 -0400

    [Data : Serialization : Hazards] Add SerializableHazardHelper

---
+++ Assets/Scripts/Data/Serialization/SerializableHazards/SerializableHazardHelper.cs
@@ -0,0 +1,16 @@
+﻿using WorldObjects.Hazards;
+
+namespace Data.Serialization.SerializableHazards
+{
+    public static class SerializableHazardHelper
+    {
+        public static SerializableHazard ToSerializableHazard(Hazard hazard)
+        {
+            if (hazard is StalagHazard)
+            {
+                return new SerializableStalag(hazard as StalagHazard);
+            }
+            else throw new System.Exception($"Unknown hazard type: {hazard.GetType().Name}.  Cannot serialize.");
+        }
+    }
+}
