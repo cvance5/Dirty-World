ff28a339cf166f365d6691f54255e755007ad295
cvance5 <32501792+cvance5@users.noreply.github.com>
2019-05-17 16:54:32 -0400

    [Math : Comparisons] Add IntVector2GridComparer and Tests

---
+++ Assets/Editor/Tests/MathTests/IntVector2GridComparerTests.cs
@@ -0,0 +1,52 @@
+﻿using MathConcepts;
+using MathConcepts.Comparisons;
+using NUnit.Framework;
+
+namespace Tests.MathTests
+{
+    public class IntVector2GridComparerTests
+    {
+        [Test]
+        public void TestComparisons()
+        {
+            var comparer = new IntVector2GridComparer();
+
+            var smallerXSameY = new IntVector2(-1, 0);
+            var largerXSameY = new IntVector2(1, 0);
+
+            Assert.AreEqual(-2, comparer.Compare(smallerXSameY, largerXSameY));
+            Assert.AreEqual(2, comparer.Compare(largerXSameY, smallerXSameY));
+
+            var smallerXLargerY = new IntVector2(-1, 1);
+            var largerXSmallerY = new IntVector2(1, -1);
+
+            Assert.AreEqual(-2, comparer.Compare(smallerXLargerY, largerXSmallerY));
+            Assert.AreEqual(2, comparer.Compare(largerXSmallerY, smallerXLargerY));
+
+            var smallerXSmallerY = new IntVector2(-1, -1);
+            var largerXLargerY = new IntVector2(1, 1);
+
+            Assert.AreEqual(-2, comparer.Compare(smallerXSmallerY, largerXLargerY));
+            Assert.AreEqual(2, comparer.Compare(largerXLargerY, smallerXSmallerY));
+
+            var sameXSmallerY = new IntVector2(0, -1);
+            var sameXLargerY = new IntVector2(0, 1);
+
+            Assert.AreEqual(-2, comparer.Compare(sameXSmallerY, sameXLargerY));
+            Assert.AreEqual(2, comparer.Compare(sameXLargerY, sameXSmallerY));
+
+            var sameXsameY = new IntVector2(0, 0);
+
+            Assert.AreEqual(0, comparer.Compare(sameXsameY, sameXsameY));
+
+            var baseline = new IntVector2(0, 0);
+            var largerByOne = new IntVector2(1, 0);
+            var smallerByOne = new IntVector2(-1, 0);
+            var largerByThree = new IntVector2(3, 2);
+
+            Assert.AreEqual(-1, comparer.Compare(baseline, largerByOne));
+            Assert.AreEqual(1, comparer.Compare(baseline, smallerByOne));
+            Assert.AreEqual(-3, comparer.Compare(baseline, largerByThree));
+        }
+    }
+}
---
+++ Assets/Scripts/MathConcepts/Comparisons/IntVector2GridComparer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace MathConcepts.Comparisons
+{
+    public class IntVector2GridComparer : Comparer<IntVector2>
+    {
+        public override int Compare(IntVector2 first, IntVector2 second)
+        {
+            if (first.X != second.X)
+            {
+                return first.X - second.X;
+            }
+            else return first.Y - second.Y;
+        }
+    }
+}
