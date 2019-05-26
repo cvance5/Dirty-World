93defca633f21eb5f7264eff00318b4c19886aad
cvance5 <32501792+cvance5@users.noreply.github.com>
2019-05-25 22:27:36 -0400

    [Utils] Add RandomOrder for Dictionary

--- Assets/Scripts/Utilities/Extensions/DictionaryExtensions.cs
+++ Assets/Scripts/Utilities/Extensions/DictionaryExtensions.cs
@@ -1,8 +1,14 @@
-﻿using System;
+﻿using MathConcepts;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public static class DictionaryExtensions
 {
+    public static Dictionary<T, U> RandomOrder<T, U>(this Dictionary<T, U> dict) => 
+        dict.OrderBy(kvp => Chance.Percent)
+            .ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+
     public static void RemoveAll<T, U>(this Dictionary<T, U> dict, Func<KeyValuePair<T, U>, bool> predicate)
     {
         var newDict = new Dictionary<T, U>(dict);
